adcac974dbabe87b7d7150965e6f2059bc17d68a
rootreboot <JoaoMdCouto@users.noreply.github.com>
2020-01-05 23:28:21 +0000

    SQL Exercise 7 done

--- SQL_exercise_07/7_questions.sql
+++ SQL_exercise_07/7_questions.sql
@@ -1,4 +1,21 @@
--- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express 
+-- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express
 -- 7.1 Who receieved a 1.5kg package?
-    -- The result is "Al Gore's Head".
--- 7.2 What is the total weight of all the packages that he sent?
+select
+  name
+from client
+join package on client.accountNumber = package.Recipient
+where
+  weight = 1.5;
+-- The result is "Al Gore's Head".
+  -- 7.2 What is the total weight of all the packages that he sent?
+select
+  sum(weight)
+from package
+where
+  sender = (
+    select
+      accountNumber
+    from client
+    where
+      name = "Al Gore's Head"
+  );
